a41b046503b3261bd5857a0360d92dc2470d382c
Krupesh Anadkat <kmanadkat@gmail.com>
2022-03-23 00:18:12 +0530

    Added React Router v6 and Navigation Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-import CategoriesWrapper from './components/categories-wrapper/categories-wrapper.component'
+import { Route, Routes } from 'react-router-dom'
+import Home from './routes/home/home.component'
+import Navigation from './routes/navigation/navigation.component'
+
+const Shop = () => {
+	return (
+		<div>
+			<h1>Shop Page</h1>
+		</div>
+	)
+}
 
 function App() {
-	return <CategoriesWrapper />
+	return (
+		<Routes>
+			<Route path='/' element={<Navigation />}>
+				<Route index element={<Home />} />
+				<Route path='shop' element={<Shop />} />
+			</Route>
+		</Routes>
+	)
 }
 
 export default App
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+import './navigation.styles.scss'
+
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
+
+const Navigation = () => {
+	return (
+		<React.Fragment>
+			<div className='navigation'>
+				<Link to='/' className='navigation--logo'>
+					<CrownLogo />
+				</Link>
+				<div className='navigation--links'>
+					<Link className='navigation--links__item' to='/shop'>
+						SHOP
+					</Link>
+				</div>
+			</div>
+			<Outlet />
+		</React.Fragment>
+	)
+}
+
+export default Navigation
